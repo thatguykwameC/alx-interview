4ed696fe9b95540a944e951ffe447d101344531f
thatguykwameC <thatguykwamec@gmail.com>
2024-07-25 18:32:21 -0400

    Pascal's triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,21 +1,30 @@
+
 #!/usr/bin/python3
-""" Solving Pascal's Triangle """
-
-
-def Calc(row, col):
-    """ Recursive call to calculate rows and columns """
-    if col == 0 or col == row:
-        return 1
-    return Calc(row - 1, col - 1) + Calc(row - 1, col)
+""" Module for Pascal's triangle """
 
 
 def pascal_triangle(n):
-    """ Calc the pascal triangle to the nth row """
+    """ Returns list of lists of ints representing Pascal’s triangle of n """
+
     if n <= 0:
         return []
 
-    p_triangle = []
-    for i in range(n):
-        row = [Calc(i, j) for j in range(i + 1)]
-        p_triangle.append(row)
-    return p_triangle
+    pascal = [[1]]
+    if n == 1:
+        return pascal
+
+    for i in range(1, n):
+        left = -1
+        right = 0
+        in_pas = []
+        for j in range(i+1):
+            num = 0
+            if left > -1:
+                num += pascal[i - 1][left]
+            if right < i:
+                num += pascal[i - 1][right]
+            left += 1
+            right += 1
+            in_pas.append(num)
+        pascal.append(in_pas)
+    return pascal
