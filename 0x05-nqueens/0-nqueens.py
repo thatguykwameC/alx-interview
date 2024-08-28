22f06a44d0328e2098b5c7231d3d67efc7629b09
thatguykwameC <thatguykwamec@gmail.com>
2024-08-28 16:53:15 -0400

    N-queens challenge

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -43,25 +43,24 @@ class NQueen:
         return self.res
 
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
+if len(sys.argv) != 2:
+    print("Usage: nqueens N")
+    sys.exit(1)
 
-    N = sys.argv[1]
+N = sys.argv[1]
 
-    try:
-        N = int(N)
-    except ValueError:
-        print("N must be a number")
-        sys.exit(1)
+try:
+    N = int(N)
+except ValueError:
+    print("N must be a number")
+    sys.exit(1)
 
-    if N < 4:
-        print("N must be at least 4")
-        sys.exit(1)
+if N < 4:
+    print("N must be at least 4")
+    sys.exit(1)
 
-    queen = NQueen(N)
-    res = queen.nQueen(1)
+queen = NQueen(N)
+res = queen.nQueen(1)
 
-    for i in res:
-        print(i)
+for i in res:
+    print(i)
