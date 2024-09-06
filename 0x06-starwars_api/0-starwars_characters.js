d5d3b749e54ea001cf2df017f1e4f92d5575ceca
thatguykwameC <thatguykwamec@gmail.com>
2024-09-05 20:24:48 -0400

    StarWars API

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,30 +1,28 @@
 #!/usr/bin/node
 /* script that prints the characters of a Star Wars movie */
 
-const req = require('request');
-const baseUrl = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
+const request = require('request');
 
-req(baseUrl, (err, res, body) => {
-  if (err) throw err;
-  if (res.statusCode !== 200) {
-    console.error('status code issue:', res.statusCode);
-    return;
-  }
-
-  charsChecker(JSON.parse(body).characters, 0);
-});
-
-function charsChecker(chars, idx) {
-  if (idx >= chars.length) return;
-
-  req(chars[idx], (err, res, body) => {
-    if (err) throw err;
-
-    if (res.statusCode === 200) {
-      console.log(JSON.parse(body).name);
+const req = (arr, i) => {
+  if (i === arr.length) return;
+  request(arr[i], (err, response, body) => {
+    if (err) {
+      throw err;
     } else {
-      console.error('Status code wrong:', res.statusCode);
+      console.log(JSON.parse(body).name);
+      req(arr, i + 1);
     }
-    charsChecker(chars, idx + 1);
   });
-}
+};
+
+request(
+  `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`,
+  (err, response, body) => {
+    if (err) {
+      throw err;
+    } else {
+      const chars = JSON.parse(body).characters;
+      req(chars, 0);
+    }
+  }
+);
